73958cce59f9c988f00cd555e7bd619dd978d896
Pedro S. Pereira <71725215+devpedrospereira@users.noreply.github.com>
2022-06-17 15:05:58 -0300

    import imagens

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
-import styles from "../styles/home.module.scss";
 import Head from "next/head";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Início | ig.news</title>
-      </Head>
-      <h1 className={styles.title}>Hello World</h1>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Início | ig.news</title>
+            </Head>
+            <h1>Hello World</h1>
+        </>
+    );
 }
