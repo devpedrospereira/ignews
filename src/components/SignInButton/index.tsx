5e01a573ae57bd492b9c5ff4ce4af6330a6e8de3
Pedro S. Pereira <71725215+devpedrospereira@users.noreply.github.com>
2022-06-21 09:03:40 -0300

    Implementation features of Authentication

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,27 +1,35 @@
 import { FaGithub } from "react-icons/fa";
-import {FiX} from 'react-icons/fi'
+import { FiX } from "react-icons/fi";
+// import {signIn, useSession} from 'next-auth/react'
+import { useSession, signIn, signOut } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    // const [session] = useSession()
+    const { data: session, status } = useSession();
 
-    return isUserLoggedIn ? (
-        <button 
-            type="button"
-            className={styles.signInButton}
-        >
-            <FaGithub color='#04d361'/>
-            Dev Pedro Pereira
-            <FiX color="#737380" className={styles.closeIcon}/>
-        </button>
-    ) : (
-        <button 
-            type="button"
-            className={styles.signInButton}
-        >
-            <FaGithub color='#eba417'/>
-            Entrar com Github
-        </button>
-    )
+    console.log(session);
+    console.log(status);
+
+    if (status === "authenticated") {
+        return (
+            <button type="button" className={styles.signInButton} onClick={() => signOut()}>
+                <FaGithub color="#04d361" />
+                {session.user.name}
+                <FiX color="#737380" className={styles.closeIcon} />
+            </button>
+        );
+    } else {
+        return (
+            <button
+                type="button"
+                className={styles.signInButton}
+                onClick={() => signIn("github")}
+            >
+                <FaGithub color="#eba417" />
+                Entrar com Github
+            </button>
+        );
+    }
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,14 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: { 
+          scope: 'read:user' //irá devolver apenas as informações basicas de perfil do usuario (name, username, email, avatar...)
+        }
+      }
+      
     }),
     // ...add more providers here
   ],
