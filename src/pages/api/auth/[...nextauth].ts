f064d593c710142e58854038159c69d508830e07
Pedro S. Pereira <71725215+devpedrospereira@users.noreply.github.com>
2022-06-22 08:44:24 -0300

    implementation of the user registration method in the database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import {query as q, query} from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import {fauna} from '../../../services/faunadb'
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -13,6 +16,51 @@ export default NextAuth({
       }
       
     }),
-    // ...add more providers here
   ],
+  callbacks: {//essa função será chamada toda vez que o usuario fazer o login. 
+    async signIn({ user, account, profile, credentials }) {
+      const {email} = user
+
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email),
+                ),
+              ),
+            ),
+            q.Create(
+              q.Collection('users'),
+              {data:{email}}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email),
+              )
+            )
+          )
+        )
+        return true
+      }
+
+      catch{
+        return false
+      }
+
+      // const isAllowedToSignIn = true
+      // if (isAllowedToSignIn) {
+      //   return true
+      // } else {
+      //   // Return false to display a default error message
+      //   return false
+      //   // Or you can return a URL to redirect to:
+      //   // return '/unauthorized'
+      // }
+
+    }
+  }
 })
