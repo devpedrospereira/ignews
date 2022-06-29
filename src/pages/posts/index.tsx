cf172db4b9ef8bf39f22bad42447c7660208589c
Pedro S. Pereira <71725215+devpedrospereira@users.noreply.github.com>
2022-06-29 06:42:29 -0300

    Implementation HTML and created styled by Posts component

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,95 @@
+import Head from "next/head";
+import styles from "./styles.module.scss";
+
+export default function Post() {
+    return (
+        <>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a href="#">
+                        <time>29 de junho de 2022</time>
+                        <strong>
+                            10 melhores séries policiais na Netflix para você
+                            desvendar os crimes
+                        </strong>
+                        <p>
+                            Sim, as histórias investigativas são uma paixão
+                            mundial no mundo dos seriados que segue firme e
+                            forte também nos streamings.
+                        </p>
+                    </a>
+
+                    <a href="#">
+                        <time>29 de junho de 2022</time>
+                        <strong>
+                            10 melhores séries policiais na Netflix para você
+                            desvendar os crimes
+                        </strong>
+                        <p>
+                            Sim, as histórias investigativas são uma paixão
+                            mundial no mundo dos seriados que segue firme e
+                            forte também nos streamings.
+                        </p>
+                    </a>
+
+                    <a href="#">
+                        <time>29 de junho de 2022</time>
+                        <strong>
+                            10 melhores séries policiais na Netflix para você
+                            desvendar os crimes
+                        </strong>
+                        <p>
+                            Sim, as histórias investigativas são uma paixão
+                            mundial no mundo dos seriados que segue firme e
+                            forte também nos streamings.
+                        </p>
+                    </a>
+
+
+                    <a href="#">
+                        <time>29 de junho de 2022</time>
+                        <strong>
+                            10 melhores séries policiais na Netflix para você
+                            desvendar os crimes
+                        </strong>
+                        <p>
+                            Sim, as histórias investigativas são uma paixão
+                            mundial no mundo dos seriados que segue firme e
+                            forte também nos streamings.
+                        </p>
+                    </a>
+
+                    <a href="#">
+                        <time>29 de junho de 2022</time>
+                        <strong>
+                            10 melhores séries policiais na Netflix para você
+                            desvendar os crimes
+                        </strong>
+                        <p>
+                            Sim, as histórias investigativas são uma paixão
+                            mundial no mundo dos seriados que segue firme e
+                            forte também nos streamings.
+                        </p>
+                    </a>
+
+                    <a href="#">
+                        <time>29 de junho de 2022</time>
+                        <strong>
+                            10 melhores séries policiais na Netflix para você
+                            desvendar os crimes
+                        </strong>
+                        <p>
+                            Sim, as histórias investigativas são uma paixão
+                            mundial no mundo dos seriados que segue firme e
+                            forte também nos streamings.
+                        </p>
+                    </a>
+                </div>
+            </main>
+        </>
+    );
+}
